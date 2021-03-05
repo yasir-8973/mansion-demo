2c58c39f2ed630d014046c6e8f0d55b3b6b2e476
yasir-8973 <mohammedyasir1771@gmail.com>
2021-03-06 01:29:49 +0530

    Test

---
+++ precache-manifest.a73c58cc15c47f41b5539c8b22fcd949.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dafd75f96da2bfefc56d7a292086ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0332eae3459b18bc4ceb",
+    "url": "/static/css/main.127c1dfa.chunk.css"
+  },
+  {
+    "revision": "035f890dcf29a6718de6",
+    "url": "/static/js/2.7a0b7aaf.chunk.js"
+  },
+  {
+    "revision": "3a29ca4de806252d49ca88f3ff2d068d",
+    "url": "/static/js/2.7a0b7aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0332eae3459b18bc4ceb",
+    "url": "/static/js/main.4d17d436.chunk.js"
+  },
+  {
+    "revision": "ee9add88d99bbc155fed",
+    "url": "/static/js/runtime-main.63ff5a51.js"
+  },
+  {
+    "revision": "4920b95ce5de325d701465d5abb0f491",
+    "url": "/static/media/Image1.4920b95c.jpeg"
+  },
+  {
+    "revision": "a8d808a08cd214b670c37bf24caf4531",
+    "url": "/static/media/Image2.a8d808a0.jpeg"
+  },
+  {
+    "revision": "aea10c32a058d08b79366611620130ab",
+    "url": "/static/media/Image3.aea10c32.jpeg"
+  },
+  {
+    "revision": "e23ad8bbcded809369637f7bbe5610a9",
+    "url": "/static/media/Image4.e23ad8bb.jpeg"
+  },
+  {
+    "revision": "80952b05ebb3a9058771f8c641d37355",
+    "url": "/static/media/mansion1.80952b05.png"
+  },
+  {
+    "revision": "d938f2276f54c1d115b8dc7d0d636827",
+    "url": "/static/media/mansion2.d938f227.png"
+  },
+  {
+    "revision": "2e4f1e0f15c880bd4c82bb7237ba120a",
+    "url": "/static/media/mansion3.2e4f1e0f.png"
+  },
+  {
+    "revision": "1f16692b4f3f4564c39699bc415e0c1e",
+    "url": "/static/media/mansionslide1.1f16692b.png"
+  },
+  {
+    "revision": "473465d7adad07d65bd8c1d6a5a0a05b",
+    "url": "/static/media/mansionslide2.473465d7.png"
+  },
+  {
+    "revision": "92c8581ba9f59fab5e0cb02b371d21e7",
+    "url": "/static/media/mansionslide3.92c8581b.png"
+  },
+  {
+    "revision": "bd7eed8c19f550a389c8d2b80271a1ce",
+    "url": "/static/media/mansionsmall1.bd7eed8c.jpg"
+  },
+  {
+    "revision": "3f34dfba5d98d5339c0f7afdfa1bf601",
+    "url": "/static/media/mansionsmall2.3f34dfba.jpg"
+  },
+  {
+    "revision": "61b6a0026080200a4e42328087794f69",
+    "url": "/static/media/mansionsmall3.61b6a002.jpg"
+  },
+  {
+    "revision": "e58f10da7e9c347bd126ccf99ca3418f",
+    "url": "/static/media/mansionsmall4.e58f10da.jpg"
+  },
+  {
+    "revision": "ca54e34105105efc172b08eb2d486499",
+    "url": "/static/media/starlogo.ca54e341.png"
+  }
+]);
