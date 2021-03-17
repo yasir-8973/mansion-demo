58f0638775e3cb7fd37543cf08b4c0103161dbea
yasir-8973 <mohammedyasir1771@gmail.com>
2021-03-17 23:17:59 +0530

    Test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7899678cf91d7475b6606c0a50f638d1.js"
+  "/precache-manifest.d13fdd824248abdd827d7694b4df33cb.js"
 );
 
 self.addEventListener('message', (event) => {
